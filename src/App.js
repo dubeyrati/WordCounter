85afe7a7002263833254170f23440c50f286b3dc
Rati Dubey <dubeyrati1@gmail.com>
2023-10-15 11:09:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react"; 
 
 function App() {
+    const [text, setText] = useState("");
+    const wordCount = text.split(/\s+/).filter(Boolean).length;
+    const handleTextChange = (e) => {
+      setText(e.target.value);
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+        <div>
+          <h1>Responsive Paragraph Word Counter</h1>
+          <textarea value={text} cols={70} rows={10} placeholder="Type here...." onChange={handleTextChange}></textarea>
+          <p>Word Count: {wordCount}</p>
+        </div>
+    </center>
   );
 }
 
